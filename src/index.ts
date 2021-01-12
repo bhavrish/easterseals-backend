a6089040092c67b1d4e8fb4ebd12573f78e621b2
bhavrish <bhavrish@gmail.com>
2021-01-12 15:15:03 -0500

    Bug fix

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import * as dotenv from 'dotenv';
 import routes from "./routes/index";
 
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 // middlewares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); 
 
 app.use(routes);
-app.listen(4000);
+app.listen(PORT);
