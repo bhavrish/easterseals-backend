44b69fb498cf485d2a3fa81bf6d5cdc3a854cb9a
bhavrish <bhavrish@gmail.com>
2021-02-23 14:43:10 -0500

    Modify database connections

--- dist/database.js
+++ dist/database.js
@@ -2,10 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.pool = void 0;
 const pg_1 = require("pg");
+const isProduction = process.env.NODE_ENV === 'production';
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 exports.pool = new pg_1.Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    port: Number(process.env.DB_PORT),
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: isProduction
 });
+// export const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT),
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+// })
--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,19 @@
 import { Pool } from 'pg'
 import * as dotenv from 'dotenv';
 
+const isProduction = process.env.NODE_ENV === 'production'
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DATABASE_PASSWORD
+}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
 export const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    port: Number(process.env.DB_PORT),
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: isProduction
 })
+
+// export const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT),
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+// })
