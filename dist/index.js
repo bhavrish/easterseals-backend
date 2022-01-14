db5b8165a550b2076681ef6b36120fe47d73242a
Chi-O <chioma.o3762@gmail.com>
2022-01-13 22:06:36 -0500

    JS file changes to run locally

--- dist/index.js
+++ dist/index.js
@@ -26,7 +26,7 @@ const express_1 = __importDefault(require("express"));
 const dotenv = __importStar(require("dotenv"));
 const index_1 = __importDefault(require("./routes/index"));
 dotenv.config();
-const app = (0, express_1.default)();
+const app = express_1.default();
 const PORT = process.env.PORT || 4000;
 // middlewares
 app.use(express_1.default.json());
